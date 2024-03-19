67e4d116c1934344299cc730f97e2d50c7865d05
Thanh Trung <trandacthanhtrungvd@gmail.com>
2024-03-19 19:06:45 +0700

    Setup path alias and install some packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
